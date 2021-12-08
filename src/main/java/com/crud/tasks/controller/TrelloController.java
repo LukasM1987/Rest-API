5184698eeb803cff94ca899f3a734f5dcef855d4
Lukasz Marchel <marchel.lukas@gmail.com>
2021-12-08 09:06:55 +0100

    KOD-22.2.1 rest template task

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
 public class TrelloController {
+
+    private static final String nameFragment = "kodilla";
 
     private final TrelloClient trelloClient;
 
@@ -20,9 +24,12 @@ public class TrelloController {
     public void getTrelloBoards() {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
         trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+            Pattern patternFragment = Pattern.compile(nameFragment);
+            Matcher matchFragment = patternFragment.matcher(trelloBoardDto.getName().toLowerCase());
+            if (trelloBoardDto.getId() != null && trelloBoardDto.getName() != null && matchFragment.find()) {
+                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+            }
         });
     }
 }
