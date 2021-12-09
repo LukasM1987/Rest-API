8102048d8bbdb8757a0c021f43800e96b1f0062d
Lukasz Marchel <marchel.lukas@gmail.com>
2021-12-09 17:14:10 +0100

    KOD-22.3 customer expansion

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,11 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -30,6 +31,15 @@ public class TrelloController {
             if (trelloBoardDto.getId() != null && trelloBoardDto.getName() != null && matchFragment.find()) {
                 System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
             }
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
         });
+    }
+
+    @PostMapping("createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
     }
 }
